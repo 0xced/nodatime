bb52007c3e2223bad476b75114b197266daa277d
Cédric Luthi <cedric.luthi@gmail.com>
2021-06-17 10:38:10 +0200

    Add .NET 6.0 target to the NodaTime project
    Also fix all related nullability warnings that appeared

--- src/NodaTime/YearMonth.cs
+++ src/NodaTime/YearMonth.cs
@@ -205,7 +205,7 @@ namespace NodaTime
         /// <returns>The result of comparing this YearMonth with another one.
         /// If <paramref name="obj"/> is null, this method returns a value greater than 0.
         /// </returns>
-        int IComparable.CompareTo(object obj)
+        int IComparable.CompareTo(object? obj)
         {
             if (obj is null)
             {
